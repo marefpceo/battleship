16bb1fc6f0b845edd9f36717590c974196bc3e03
marefpceo <marefpceo@gmail.com>
2023-03-30 16:14:19 -0700

    Initial commit
    Completed webpack setup.
    Installed eslint and jest, both with config files.

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  output: {
+    clean: {
+      keep: /index.html/,
+    },
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: './assets/[name][ext][query]',
+    publicPath: '',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      }
+    ],
+  },
+};
